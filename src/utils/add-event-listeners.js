eeac6a14d5daf39b1edc4605f27d763df56a4bba
NSkye <nikolai.laevskii@yandex.ru>
2020-06-01 03:22:53 +0300

    Disable passive events

--- src/utils/add-event-listeners.js
+++ src/utils/add-event-listeners.js
@@ -9,6 +9,9 @@ import splitStr from './split-str';
  */
 export default function addEventListeners(target, types, handler) {
   each(splitStr(types), (type) => {
-    target.addEventListener(type, handler, false);
+    target.addEventListener(type, handler, {
+      capture: false,
+      passive: false
+    });
   });
 }
